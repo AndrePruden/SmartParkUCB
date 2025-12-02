4925d0d492b26f75bac9bd8a36d7149fe5b31240
Carlos Prudencio <andrepruden0@gmail.com>
2025-12-01 22:43:07 -0400

    Errores Arreglados

--- app/src/main/java/com/ucb/smartpark/ui/theme/Color.kt
+++ app/src/main/java/com/ucb/smartpark/ui/theme/Color.kt
@@ -10,6 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-// COLORES A USAR
-val UcbYellow = Color(0xFFFDB813)
+// El amarillo UCB que usaste en el Login
+val UcbYellow = Color(0xFFFFD600) // Amarillo intenso
 val UcbBlue = Color(0xFF003366)
